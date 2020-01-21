f2d4ef8c5d2c439c58d1969dcad9c1baeac2a5e9
Charlie Pitkin <charlie@wearefine.com>
2020-01-21 22:39:35 +0200

    Added main deploy files
    Added readme
    Added static deploy
    Added Slack notify
    
    Signed-off-by: Charlie Pitkin <charlie@wearefine.com>

--- README.md
+++ README.md
@@ -1,1 +1,68 @@
-static-site-jenkins-shared-libraries
+# static-site-jenkins-shared-libraries
+
+Putting CI/CD on your static site on every change should be a smooth process. With Jenkins pipelines you can describe the entire process through code. We did the hard work to make our static site deployment library open source so you can benefit. It gives you a drop in pipeline shared library with a configurable Jenkinsfile.
+
+## Prerequisites
+
+### Jenkins
+
+If you're new to Jenkins pipelines you should go read the [documentation](https://jenkins.io/doc/book/pipeline/) before proceeding to get a sense for what to expect using this code. The rest of the setup process will assume you have basic knowledge of Jenkins or CI/CD jobs in general.
+
+#### OS
+
+- rvm installed in the jenkins user
+- git
+- build-essential
+
+#### Jenkins
+
+- Version: > 2.7.3 - tested on (2.150.2 LTS)
+
+#### Plugins
+
+- slack
+- pipeline (workflow-aggregator)
+- git
+- timestamper
+- credentials
+- docker
+- AWS ECR credentials
+
+#### Scripts Approval
+
+When the job runs the first time you will need to work through allowing certain functions to execute in the groovy sandbox. This is normal as not all high use groovy functions are in the default safelist but more are added all the time.
+
+## Jenkinsfile
+
+``` groovy
+static {
+
+  NODE_INSTALL_NAME = 'lts/boron'
+  SLACK_CHANNEL = '#deploys'
+  DEBUG = 'false'
+  DOCKER_REGISTRY_CREDS_ID = 'access_docker_hub'
+  DOCKER_REGISTRY_URL = 'https://hub.docker.io'
+  AWS_DEFAULT_REGION = 'us-east-1'
+  AWS_CREDS_ID  = 'aws_creds_id'
+  PROD_BUCKET = 'bucket-name'
+  DEV_BUCKET = 'bucket-name-dev'
+  DISTRIBUTION = '123ABC'
+  NODE_MAJOR_VERSION = '12'
+}
+```
+
+### Required Parameters
+
+- **NODE_INSTALL_NAME:** Nodejs plugin uses names to identify installs in Jenkins, enter that value here [String]
+- **DOCKER_REGISTRY_URL:** The private Docker registry URL. Required to build with Docker. [String]
+- **DOCKER_REGISTRY_CREDS_ID:** The private Docker registry credentials ID in Jenkins. Required to build with Docker. [String]
+- **AWS_DEFAULT_REGION:** The AWS region of you Elastic Container Registry [String]
+- **AWS_CRED_ID:** The AWS credentials ID from Jenkins to push files to AWS S3 [String]
+- **PROD_BUCKET:** The AWS S3 bucket to deploy production assets into [String]
+- **DEV_BUCKET:** The AWS S3 bucket to deploy development assets into [String]
+- **DISTRIBUTION:** The Cloudfront distribution to invalidate on deploy [String]
+- **NODE_MAJOR_VERSION:** The major version of Nodejs to use for the build container [String]
+
+**Note:** The current setup only works with AWS ECR but can easily be adapted to work with other private registries.
+
+## [MIT License](LICENSE)
---
+++ var/static.groovy
@@ -0,0 +1,110 @@
+#!/usr/bin/env groovy
+
+def call(body) {
+  // evaluate the body block, and collect configuration into the object
+  def config = [:]
+  body.resolveStrategy = Closure.DELEGATE_FIRST
+  body.delegate = config
+  body()
+
+  if (!config.DEBUG){
+    env.DEBUG = 'false'
+  }
+  if (!config.SLACK_CHANNEL){
+    env.SLACK_CHANNEL = '#deploys'
+  }
+   if (!config.DOCKER_REGISTRY_CREDS_ID) {
+    error 'DOCKER_REGISTRY_CREDS_ID is required to use Docker builds'
+  }
+  if (!config.DOCKER_REGISTRY_URL){
+    error 'DOCKER_REGISTRY_URL is required to use Docker builds'
+  }
+  if (!config.AWS_DEFAULT_REGION){
+    env.AWS_DEFAULT_REGION = 'us-west-2'
+  } else {
+    env.AWS_DEFAULT_REGION = config.AWS_DEFAULT_REGION
+  }
+  if (!config.AWS_CREDS_ID){
+    error 'AWS_CREDS_ID is required'
+  }
+  if (!config.PROD_BUCKET){
+    error 'PROD_BUCKET is required'
+  }
+  if (!config.DEV_BUCKET){
+    error 'DEV_BUCKET is required'
+  }
+  if (!config.DISTRIBUTION){
+    error 'DISTRIBUTION is required'
+  }
+  if (!config.NODE_MAJOR_VERSION){
+    error 'NODE_MAJOR_VERSION is required to use Docker builds'
+  }
+
+  node {
+    timestamps {
+      if (env.DEBUG == 'false') {
+        notifySlack(env.SLACK_CHANNEL)
+      }
+
+      try {
+        stage('Checkout') {
+          checkout scm
+          currentBuild.result = 'SUCCESS'
+        }
+      } catch(Exception e) {
+        currentBuild.result = 'FAILURE'
+        if (env.DEBUG == 'false') {
+          notifySlack(env.SLACK_CHANNEL)
+        }
+        throw e
+      }
+
+      docker.withRegistry(config.DOCKER_REGISTRY_URL, "ecr:${env.AWS_DEFAULT_REGION}:${config.DOCKER_REGISTRY_CREDS_ID}") {
+
+        docker.image("${config.DOCKER_REGISTRY}:${config.NODE_MAJOR_VERSION}-dev").inside(containerArgs) {
+          try {
+            stage('Install Dependencies'){
+              milestone label: 'Install Dependencies'
+
+              retry(2) {
+                sh 'yarn --no-progress --non-interactive --frozen-lockfile'
+              }
+
+              currentBuild.result = 'SUCCESS'
+
+            }
+          } catch(Exception e) {
+            currentBuild.result = 'FAILURE'
+            notifySlack(env.SLACK_CHANNEL)
+            throw e
+          }
+
+          try {
+            stage('Deploy'){
+              milestone label: 'Deploy'
+
+              withAWS(credentials: config.AWS_CREDS_ID, region: config.AWS_DEFAULT_REGION) {
+                if (env.BRANCH_NAME == 'master') {
+                  s3Upload(file: 'dist', bucket: env.PROD_BUCKET, path: "custom/")
+                  cfInvalidate(distribution: env.DISTRIBUTION, paths:['/custom/*'])
+                }
+                else if (env.BRANCH_NAME == 'dev') {
+                  s3Upload(file: 'dist', bucket: env.DEV_BUCKET, path: "/custom/")
+                }
+              }
+            }
+            currentBuild.result = 'SUCCESS'
+          } catch(Exception e) {
+            currentBuild.result = 'FAILURE'
+            if (env.DEBUG == 'false') {
+              notifySlack(env.SLACK_CHANNEL)
+            }
+            throw e
+          }
+        } // withRegistry
+      } // timestamps
+    if (env.DEBUG == 'false') {
+      notifySlack(env.SLACK_CHANNEL)
+    }
+  } // node
+}
